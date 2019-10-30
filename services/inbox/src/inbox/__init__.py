c05ef0645df513da1e4bcf8b39a8fbb842915e37
Martin Zackrisson <martin.zackrisson@fripost.org>
2019-10-30 18:53:57 +0100

    Putting mails in mongo

--- services/inbox/src/inbox/__init__.py
+++ services/inbox/src/inbox/__init__.py
@@ -1,18 +1,32 @@
 import os
+
+from pymongo import MongoClient
+from pymongo.database import Database
+
 from .mailgateway import get_server
+from .mongogateway import insert_mail
 
-host = os.environ.get("EUCO_MAIL_SERVER_HOST", "")  # Defaults to localhost
-port = int(os.environ.get("EUCO_MAIL_SERVER_PORT", 143))
-user = os.environ.get("EUCO_MAIL_USER", "noone")
-password = os.environ.get("EUCO_MAIL_PWD", "nopwd")
+HOST = os.environ.get("EUCO_MAIL_SERVER_HOST", "")  # Defaults to localhost
+PORT = int(os.environ.get("EUCO_MAIL_SERVER_PORT", 143))
+USER = os.environ.get("EUCO_MAIL_USER", "noone")
+PASSWORD = os.environ.get("EUCO_MAIL_PWD", "nopwd")
+MONGO_URI = os.environ.get("EUCO_MONGO_URI", "mongodb://mongodb")
+MONGO_DB = os.environ.get("EUCO_MONGO_DB", "euco")
 
 
-def process_mails(host: str, port: int, user: str, password: str):
+def get_db(uri: str, db: str) -> Database:
+    mongo_client = MongoClient(uri)
+    return mongo_client[db]
+
+
+def process_mails(
+    host: str, port: int, user: str, password: str, db: Database,
+):
     server = get_server(host, port)
     for mail in server.get_messages(user, password):
-        print(mail)
-        print("---")
+        insert_mail(db, mail)
 
 
 if __name__ == '__main__':
-    process_mails(host, port, user, password)
+    db = get_db(MONGO_URI, MONGO_DB)
+    process_mails(HOST, PORT, USER, PASSWORD, db)
