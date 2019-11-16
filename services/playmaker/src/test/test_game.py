9e9a9725f9c1abfb17ea7ac599564543b8d349dd
Martin Zackrisson <martin.zackrisson@fripost.org>
2019-11-16 10:31:10 +0100

    some more tests to make sure game logic works

--- services/playmaker/src/test/test_game.py
+++ services/playmaker/src/test/test_game.py
@@ -4,6 +4,20 @@ from playmaker import game
 from playmaker import models
 
 
+def mkcommunication(
+    mongodb_id="aaaaaa",
+    mailer="hello@world.org",
+    name="Hello Me",
+    reciever="support@euco.com",
+    body="What is this crap?",
+    medium="mail",
+):
+    return models.Communication(
+        mongodb_id=mongodb_id, mailer=mailer, name=name, reciever=reciever,
+        body=body, medium=medium,
+    )
+
+
 @pytest.mark.parametrize("text,profile,expect", (
     (
         "Hello {{profile.name}}!",
@@ -23,3 +37,32 @@ from playmaker import models
 ))
 def test_format_response(text, profile, expect):
     assert game.format_response(text, profile) == expect
+
+
+class TestGetEvolvedProfile:
+    def test_applies_name(self):
+        profile = models.Profile(mailer="me@me.me")
+        bundle = [mkcommunication(name="I am Name")]
+        assert game.get_evolved_profile(bundle, profile) == models.Profile(
+            name="I am Name",
+            mailer="me@me.me",
+        )
+
+    def test_updates_name_if_probable(self):
+        profile = models.Profile(mailer="me@me.me", name='Dot')
+        bundle = [
+            mkcommunication(name="I am Name"), mkcommunication(name="I am Name"),
+            mkcommunication(name="Dot"),
+        ]
+        assert game.get_evolved_profile(bundle, profile) == models.Profile(
+            name="I am Name",
+            mailer="me@me.me",
+        )
+
+    def test_keeps_name(self):
+        profile = models.Profile(mailer="me@me.me", name="Dot")
+        bundle = [mkcommunication(name="I am Name")]
+        assert game.get_evolved_profile(bundle, profile) == models.Profile( 
+            name="Dot",
+            mailer="me@me.me",
+        )
