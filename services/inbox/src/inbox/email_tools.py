1a2fa8158e6f3fcba0844db73b22ed61f9b6aaa8
Martin Zackrisson <martin.zackrisson@fripost.org>
2019-11-23 11:33:26 +0100

    decoding

--- services/inbox/src/inbox/email_tools.py
+++ services/inbox/src/inbox/email_tools.py
@@ -62,10 +62,10 @@ def get_data_object_from_mail(
             ctype = part.get_content_type()
             cdispo = str(part.get('Content-Disposition'))
             if ctype == 'text/plain' and 'attachment' not in cdispo:
-                data['body'] = part.get_payload(decode=True)
+                data['body'] = part.get_payload(decode=True).decode()
                 break
     else:
-        data['body'] = mail.get_payload(decode=True)
+        data['body'] = mail.get_payload(decode=True).decode()
     if isinstance(data['body'], bytes):
         data['body'] = data['body'].decode()
     return data
